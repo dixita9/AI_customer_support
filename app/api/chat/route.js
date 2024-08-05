28a9b66c3d55bd9c6e8668a58e2d003e76dd5455
gaonaborgesa <gaonaborgesa@berea.edu>
2024-08-05 15:00:25 -0400

    added return statement

--- app/api/chat/route.js
+++ app/api/chat/route.js
@@ -1,16 +1,18 @@
 import OpenAI from "openai";
+import {NextResponse} from 'next/server'
 
-export default function POST(){
-    const openai = new OpenAI();
-    const completion = await openai.chat.completions.create({
-        messages: [{"role": "system", "content": "You are a helpful assistant."},
-            {"role": "user", "content": "Who won the world series in 2020?"},
-            {"role": "assistant", "content": "The Los Angeles Dodgers won the World Series in 2020."},
-            {"role": "user", "content": "Where was it played?"}],
-        model: "gpt-4o-mini",
-      });
+export async function POST(req){
+    // const openai = new OpenAI();
+    // const completion = await openai.chat.completions.create({
+    //     messages: [{"role": "system", "content": "You are a helpful assistant."},
+    //         {"role": "user", "content": "Who won the world series in 2020?"},
+    //         {"role": "assistant", "content": "The Los Angeles Dodgers won the World Series in 2020."},
+    //         {"role": "user", "content": "Where was it played?"}],
+    //     model: "gpt-4o-mini",
+    //   });
     
-      console.log(completion.choices[0]);
+    //   console.log(completion.choices[0]);
+      return NextResponse.json({message:'Hello from the server'})
     
 }
 
