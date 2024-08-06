7986dca1f5cb02368e917873f647443ce35d19ce
Anupriya Dixit <dixita@berea.edu>
2024-08-06 15:38:02 -0400

    designed the textfield and button

--- app/page.js
+++ app/page.js
@@ -1,8 +1,27 @@
-import Image from "next/image";
-
+"use client"
+import { Button, Box, Stack, TextField} from "@mui/material";
+import {useState} from "react"
 
 export default function Home() {
+  const [messages, setMessages] = useState([
+    {role: 'assistant' , content : `Hi! I'm the Headstarter support assistant. How can I help you today?`}
+  ])
   return (
-    
+    <Box width = "100vh"
+        height = "100vh"
+        display = "flex"
+        flexDirection = "column"
+        justifyContent = "flex-end"
+    >  
+    <Stack>
+
+      <Stack>
+        <Stack direction = {"row"} spacing = {2}>
+          <TextField label = "Message" fullWidth />
+          <Button variant = "contained">Send</Button>
+        </Stack>
+      </Stack>
+    </Stack>
+    </Box>
   );
 }
