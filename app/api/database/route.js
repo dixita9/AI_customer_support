1eabe5ad03de5aa21343899318e0210db52ee899
hoerstl <hoerstl@berea.edu>
2024-08-11 16:22:55 -0400

    Added some packages and added a route which tests our database

--- app/api/database/route.js
+++ app/api/database/route.js
@@ -1,26 +1,36 @@
 import { NextResponse } from 'next/server';
-// const { ChromaClient } = require("chromadb");
-// console.log("hello from database")
-// const client = new ChromaClient();
+const { ChromaClient } = require("chromadb");
+console.log("hello from database")
+const client = new ChromaClient();
 
-// const collection = await client.createCollection({
-//     name: "my_collection",
-// });
+await client.reset() 
 
-// await collection.add({
-//     documents: [
-//         "This is a document about pineapple",
-//         "This is a document about oranges"
-//     ],
-//     ids: ["id1", "id2"],
-// });
+console.log("passed the reset")
 
-// const results = await collection.query({
-//     queryTexts: ["This is a query document about hawaii"], // Chroma will embed this for you
-//     nResults: 2, // how many results to return
-// });
+const collection = await client.createCollection({
+    name: "my_collection",
+});
 
-// console.log(results)
+console.log("Created a collection")
+
+await collection.add({
+    documents: [
+        "This is a document about pineapple",
+        "This is a document about oranges"
+    ],
+    ids: ["id1", "id2"],
+});
+
+console.log("Created some documents")
+
+const results = await collection.query({
+    queryTexts: ["This is a query document about hawaii"], // Chroma will embed this for you
+    nResults: 2, // how many results to return
+});
+
+console.log("Gathered the results! Here they are:")
+
+console.log(results)
 
 export async function GET(req){
     return NextResponse.json(
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    webpack: (config, { isServer }) => {
+      // Add rule for .node files
+      config.module.rules.push({
+        test: /\.node$/,
+        use: 'node-loader',
+      });
+  
+      return config;
+    },
+  };
+  
+  export default nextConfig;
